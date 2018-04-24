93a9299c6cd7601ed034ece6abdaadc4dcf20f00
Michal <mwpodgorni@gmail.com>
2018-04-24 23:45:36 +0200

    System
    I have created a skeleton of our system

---
+++ Code/AirlineSystem/src/Controller/LogInController.java
@@ -0,0 +1,25 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LogInController {
+    @FXML
+    private AnchorPane anchorPane;
+
+    public void LogInButtonController() throws IOException {
+        Stage stage = (Stage) anchorPane.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../View/FXML/Administrator/Main.fxml"))));
+    }
+
+    public void SkipButtonController() throws IOException {
+        Stage stage = (Stage) anchorPane.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../View/FXML/User/Main.fxml"))));
+    }
+
+}

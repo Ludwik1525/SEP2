365bd2059cd47acc7575a225379b130759440ba9
Michal <mwpodgorni@gmail.com>
2018-05-12 12:36:29 +0200

    calculators
    two versions

---
+++ Assignments/SWE_Assignment/Calculator/src/Model/Model.java
@@ -0,0 +1,20 @@
+package Model;
+
+public class Model {
+
+    public double calculate(double number1, double number2, String operator){
+        switch (operator){
+            case "+": return number1+number2;
+            case "-": return number1-number2;
+            case "*": return number1*number2;
+            case "/":
+                if(number2==0){
+                    return 0;
+                }
+                return number1/number2;
+        }
+        System.out.println("Unknown operator");
+
+        return 0;
+    }
+}

c710ac34dcd0e7777da47f439668b8363df84c9f
Dragos Sirbu <dragsirbu@gmail.com>
2018-05-20 15:01:25 +0200

    Finished code model

--- SEP/Code/AirlineSystem/src/Domain/Model/Flight.java
+++ SEP/Code/AirlineSystem/src/Domain/Model/Flight.java
@@ -7,4 +7,53 @@ public class Flight {
     private Date departureTime;
     private Date arrivalTime;
     private  String status;
+
+    public Flight(String number, Date departureTime, Date arrivalTime, String status) {
+        this.number = number;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+        this.status = status;
+    }
+
+    public Flight() {
+
+    }
+
+    public void setArrivalTime(Date arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public void setDepartureTime(Date departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public Date getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public Date getDepartureTime() {
+        return departureTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    @Override
+    public String toString() {
+        return  "Flight number: "+getNumber()+"; Departure time: "+getDepartureTime()+"; Arrival time: "+
+                getArrivalTime()+"; Status: "+getStatus();
+    }
 }
